f20b37df7e62a5de1c01234f5e2f36877df75c3e
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-07-28 09:46:43 +0530

    Added Navbar Structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Logo from "./../../public/logo.png";
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <nav className="flex items-center justify-between md:justify-around gap-0 md:gap-15 p-5">
+        <div className="flex items-center gap-1 cursor-pointer">
+          <img src={Logo} alt="Logo" className="w-8" />
+          <h1 className="text-3xl font-extrabold font-sans">Hacknity</h1>
+        </div>
+        <div className="flex items-center justify-between w-200  text-lg font-semibold">
+          <ul className="flex gap-8 text-gray-500">
+            <li className=" cursor-pointer">Home</li>
+            <li className=" cursor-pointer">Hackathon</li>
+            <li className=" cursor-pointer">About</li>
+            <li className=" cursor-pointer">Dashboad</li>
+          </ul>{" "}
+          <p className=" cursor-pointer">Login</p>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
