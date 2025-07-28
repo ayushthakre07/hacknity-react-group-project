e75ee0f904026f665f349d8e570615da3d58279b
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-07-28 21:22:38 +0530

    card folder added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Button from '../components/Button'
 import About from '../views/About'
 import Dashboard from '../views/Dashboard'
+import Navbar from '../components/Navbar'
 function Home() {
   return (
     <div>
+      <Navbar/>
       <Dashboard/>
   <About/>
     <h1>Home Page</h1>
