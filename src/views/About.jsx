fb2acfe1fa3d1a71319f9b227d950b0e2ccb7ea6
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-07-30 14:42:03 +0530

    Lucide icon added

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import Button from '../components/Button'
+
 function About() {
+
+
   return (
     
     <div>
       
-      <p className='text-[60px] text-center font-bold'>Versatile team masters every <span className='text-lime-500'>challenge!</span></p>
+      <p className='text-[60px] text-center font-bold'>Versatile team masters every <span className='text-lime-500 transition-colors'>challenge!</span></p>
     <div className='text-[40px] text-center text-gray-500 font-semibold pt-4'>Our Mission: Empowering Builders</div>
     <p className='text-2xl text-center px-2 py-10 text-gray-400'>You have the potential to build something that can change the world. We're here to make sure you get your shot. Our project, 
       <span className='cursor-pointer text-lime-500 font-bold'> <a href='https://github.com/Utkarshende/hacknity-react-group-project'target='_blank'>HACKNITY</a></span>, 
       creates opportunities for builders like you. We're excited to help bring your ideas to life.</p>
    
    
-    <div className='place-items-end-safe pt-[40px] flex flex-wrap px-[50px] py-[50px] border-2 ml-[200px] mr-[200px]' >
-      <div className='border-2 border-amber-500 px-4 py-4 rounded-2xl ml-[20px]'>
+    <div className='place-items-end-safe pt-[40px] flex flex-wrap px-[50px] py-[50px] ml-[200px] mr-[200px]' >
+    <div className='border-2 border-gray-200 px-4 py-4 rounded-2xl ml-[20px]'>
      <img src='https://avatar.iran.liara.run/public/21' className='size-44  ml-1 '/>
-      <p className='py-4 px-10 bg-amber-400 mt-[-10px] rounded-lg'>Ayush Thakre</p>
+      <p className='py-4 px-10 bg-amber-300 mt-[-10px] rounded-lg'>Ayush Thakre</p>
       <p  className='px-10'>UI/UX Designer</p>
       <div className='flex mt-4 ml-4'>
       <a href='https://www.instagram.com/' target='_blank'>
-       <img src='https://cdn-icons-png.flaticon.com/128/1384/1384031.png ' className='size-8 bg-gray-300 py-1 px-1 rounded-xl'/></a>
+       <img src='https://cdn-icons-png.flaticon.com/128/1384/1384031.png ' className='size-8 bg-gray-200 p-1 rounded-xl transition-transform duration-200 hover:scale-120 hover:bg-gray-300'/></a>
        <a href='' target='_blank'>
-       <img src='https://cdn-icons-png.flaticon.com/128/1006/1006771.png' className='size-8 bg-gray-300 py-1 px-1 rounded-xl ml-8'/>  
+       <img src='https://cdn-icons-png.flaticon.com/128/1006/1006771.png' className='size-8 bg-gray-200 py-1 px-1 rounded-xl ml-8'/>  
        </a>
        <a href='' target='_blank'>
-       <img src='https://cdn-icons-png.flaticon.com/128/646/646094.png' className='size-8 bg-gray-300 py-1 px-1 rounded-xl ml-8'/></a>
+       <img src='https://cdn-icons-png.flaticon.com/128/646/646094.png' className='size-8 bg-gray-200 py-1 px-1 rounded-xl ml-8'/></a>
       
       </div>
       </div>
 
-      <div className='border-2 border-amber-500 px-4 py-4 rounded-2xl ml-[150px]'>
+      <div className='border-2 border-gray-200 px-4 py-4 rounded-2xl ml-[150px]'>
      <img src='https://avatar.iran.liara.run/public/21' className='size-44'/>
       <p className='py-4 px-10 bg-amber-400 mt-[-10px] rounded-lg '>Ayush Thakre</p>
       <p  className='px-10'>UI/UX Designer</p>
@@ -43,7 +46,7 @@ function About() {
       </div>
       </div>
 
-       <div className='border-2 border-amber-500 px-4 py-4 rounded-2xl ml-[150px]'>
+       <div className='border-2 border-gray-200 px-4 py-4 rounded-2xl ml-[150px]'>
      <img src='https://avatar.iran.liara.run/public/21' className='size-44'/>
       <p className='py-4 px-10 bg-amber-400 mt-[-10px] rounded-lg '>Ayush Thakre</p>
       <p  className='px-10'>UI/UX Designer</p>
