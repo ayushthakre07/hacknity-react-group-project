c60d5d3fd4db2702b878599371a420b73a7c1efc
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-07-31 06:55:03 +0530

    Changes made in About page and footer page created

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Button from '../components/Button'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer';
 
 function About() {
 
  const teamMembers = [
-    
       {
       name: 'Gajanan Jadhav', 
       role: 'Manager', 
@@ -74,52 +73,55 @@ function About() {
     <div>
       <Navbar/>
       
-      <p className='text-[60px] text-center font-bold'>Versatile team masters every <span className='text-lime-500 transition-colors'>challenge!</span></p>
-    <div className='text-[40px] text-center text-gray-500 font-semibold pt-4'>Our Mission: Empowering Builders</div>
-    <p className='text-2xl text-center px-2 py-10 text-gray-400'>At HACKNITY, our vision is to democratize game hosting,
-       making it accessible and empowering for everyone. We believe that creativity knows no bounds, and every game, 
-      big or small, deserves a seamless and reliable platform to reach its audience.</p>
+      <p className='text-[80px] text-center font-bold'>Versatile team masters every <span className='text-lime-500 transition-colors'>challenge!</span></p>
+    
    <p>
-    <h1 className='text-6xl mx-14 mb-6 font-extralight'>Our mission is to provide an<br/>
-       intuitive, robust and community-driven platform <br/>for both <span className='text-fuchsia-400 font-extrabold'>online </span> and <span className='text-indigo-400 font-extrabold'>offline</span> game hosting.<br/> We are committed to:</h1>
-<ol className='ml-6 my-6 text-[25px] font-medium'>
-<li className="py-2"><span className='text-4xl font-bold text-amber-400'> Empowering Creators:</span>  Offering tools and resources that simplify the hosting process, allowing developers to focus on what they do best – making great games.
+    <h1 className='text-6xl my-4 text-center text-gray-400'>Our mission is to provide an<br/>
+       intuitive, robust and community-driven platform <br/>for both 
+        <span className='text-fuchsia-400 font-extrabold hover:text-fuchsia-500'>   online </span> 
+        and <span className='text-indigo-400 font-extrabold hover:text-indigo-500'>offline</span> game hosting.</h1>
+        <br/>
+        <hr className='border-dotted'/>
+       <br/> <h2 className='text-6xl font-bold text-center'> We are committed to </h2>
+<ol className='ml-6 my-6 text-[25px] font-light'>
+<li className="py-2 text-gray-500"><span className='text-4xl font-bold text-lime-500'> Empowering Creators:</span><br/>  Offering tools and resources that simplify the hosting process, allowing developers to focus on what they do best - making great games.
 </li>
-<li className="py-2 "><span className='text-4xl font-bold text-amber-400'>Enhancing Player Experience:</span> Ensuring stable, low-latency, and diverse gaming environments for players worldwide.
+<li className="py-2 text-gray-500 "><span className='text-4xl font-bold text-lime-500'>Enhancing Player Experience:</span> <br/> Ensuring stable, low-latency, and diverse gaming environments for players worldwide.
  </li>
- <li className="py-2">
-<span className='text-4xl font-bold text-amber-400'>Fostering Community: </span>Building a supportive ecosystem where developers can share, learn, and grow, and players can discover new favorites and connect with fellow enthusiasts.
+ <li className="py-2  text-gray-500">
+<span className='text-4xl font-bold text-lime-500'>Fostering Community: </span> <br/>Building a supportive ecosystem where developers can share, learn, and grow, and players can discover new favorites and connect with fellow enthusiasts.
  </li>
 </ol>
 </p>
-<hr/>
-<br/>
-<div >
-  <h1 className='text-6xl font-extrabold text-center outline-6'>What We Offer</h1>
-  <br/>
-  <h2 className='text-center text-4xl font-bold '> HACKNITY is designed to cater to a wide spectrum of hosting needs. 
-    <br/>
-    <br/>Here's what makes us the ideal choice for your game hosting:</h2>
-    
-  <br/>
-<hr  className='border-dotted'/>
-<br/>
-  <h1  className='text-4xl font-extrabold text-center '>For Online Games</h1><br/>
-  <p>Robust Server Infrastructure: We utilize state-of-the-art servers located globally to ensure minimal latency and maximum uptime for your online games. Our infrastructure is built for scalability, capable of handling sudden spikes in player numbers without a hitch.
-
-<p>Easy Deployment & Management: Our user-friendly dashboard allows for quick game uploads, server configuration, and real-time monitoring. You don't need to be a server expert to get your game online.
-</p>
-<p>Customizable Server Options: Tailor your server settings to match your game's unique requirements. From player limits to custom rules, you have full control.</p>
-</p>
-<br/>
 <hr className='border-dotted'/>
 <br/>
-<h1  className='text-4xl font-extrabold text-center '>
+<div >
+  <h1 className='text-6xl font-extrabold text-center '>What We Offer</h1>
+  <br/>
+  <h2 className='text-center text-[45px] font-bold text-gray-600'> HACKNITY is designed to cater to a wide spectrum of hosting needs. </h2>
+    <br/><span className='text-4xl text-gray-500 ml-9 font-medium'>Here's what makes us the ideal choice for your game hosting :</span>
+    
+ <br/>
+
+<br/>
+  <h1  className='text-4xl font-extrabold ml-9 text-lime-500'>For Online Games</h1><br/>
+ <div className='text-2xl ml-8 text-gray-500 font-light'>
+ <p ><span  className=' text-black '>Robust Server Infrastructure:</span><br/> Our infrastructure is built for scalability, capable of handling sudden 
+  spikes in player numbers without a hitch.</p>
+
+<p><span className=' text-black '>Easy Deployment & Management:</span> <br/>Our user-friendly dashboard allows for quick game uploads, 
+  server configuration, and real-time monitoring. You don't need to be a server expert to get your game online.</p>
+
+ </div> 
+<br/>
+<h1  className='text-4xl font-extrabold ml-9 text-lime-500'>
   For Offline Games
 </h1>
 <br/>
-<p> Secure Content Delivery: Even for offline games, secure and efficient content delivery is crucial. We provide reliable hosting for your game files, ensuring players can download your game quickly and safely.</p>
-<p>Version Control & Updates: Easily manage different versions of your game and push updates to your player base with our streamlined system</p>
+<div className='text-2xl ml-8  text-gray-500 font-light'>
+<p><span className=' text-black '> Secure Content Delivery: </span> Even for offline games, secure and efficient content delivery is crucial. We provide reliable hosting for your game files, ensuring players can download your game quickly and safely.</p>
+<p><span className=' text-black '> Version Control & Updates:</span> Easily manage different versions of your game and push updates to your player base with our streamlined system.</p>
+</div>
 <br/>
 <hr/>
 <br/>
@@ -145,28 +147,10 @@ function About() {
         ))}
       </div>
       </div>
-      ))}
-
+      ))}    
       </div>
-
-
-      <div className='text-center mt-4 text-[60px] font-extrabold text-gray-800'>Join us in our <span className='text-lime-500'>adventures</span> </div>
-      <p className='text-center mt-4 text-[30px] mx-14 text-gray-600'>We invite you to be a part of the HACKNITY journey. Whether you're launching your dream game, discovering new worlds,
- or simply connecting with fellow enthusiasts, 
-you'll find a welcoming home here. Explore our features,
- get started with your first host, or reach out to our team – we're excited to have you!</p>
-      <div className='text-center mt-4'>
-      <Button BtnTitle={"Get in touch"} />
+      <Footer/>
       </div>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <div className='text-7xl font-bold ml-[150px]'>
-        We love <span className='text-blue-300'>software</span> <br/>and the <span className='text-orange-300'>people</span>  who<br/> build them.
-      </div>
-    </div>
   )
 }
 
