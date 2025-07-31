6e8163ff5e9e712f28db403f9618324525ff68f9
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-07-31 12:16:48 +0530

    img added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,6 +6,7 @@ import { UserIcon, MailIcon } from "lucide-react";
 import HomeImg from "../../public/Home-1.jpg";
 import JoinUs from "../../public/Join-Us.png";
 import RightArrow from "../../public/right-arrow.png";
+import Pic1 from "../../public/pic-1.png";
 
 function Home() {
   const [name, setName] = useState("");
@@ -17,7 +18,7 @@ function Home() {
       alert("Please fill out both Name and Email.");
       return;
     }
-    navigate("/Login.jsx"); 
+    navigate("/Login.jsx");
   };
 
   return (
@@ -25,17 +26,36 @@ function Home() {
       <Navbar />
 
       <div className="relative">
-        <img src="/schedule.png" className="h-20 animate-bounce opacity-65 duration-75 rotate-350 absolute left-60" />
-        <img src="/software.png" className="h-20 animate-bounce opacity-65 duration-75 rotate-5 absolute right-50" />
-        <img src="/goal.png" className="h-20 animate-bounce opacity-65 duration-75 rotate-350 absolute bottom-5 left-60" />
-        <img src="/collaboration.png" className="h-25 animate-bounce opacity-65 duration-75 rotate-5 absolute right-50 bottom-0" />
-        <img src="/ideas.png" className="h-20 animate-bounce opacity-55 duration-75 rotate-5 absolute right-20 bottom-60" />
-        <img src="/brainstorm.png" className="h-25 animate-bounce opacity-65 duration-75 rotate-350 absolute bottom-60 left-20" />
+        <img
+          src="/schedule.png"
+          className="h-20 animate-bounce opacity-65 duration-75 rotate-350 absolute left-60"
+        />
+        <img
+          src="/software.png"
+          className="h-20 animate-bounce opacity-65 duration-75 rotate-5 absolute right-50"
+        />
+        <img
+          src="/goal.png"
+          className="h-20 animate-bounce opacity-65 duration-75 rotate-350 absolute bottom-5 left-60"
+        />
+        <img
+          src="/collaboration.png"
+          className="h-25 animate-bounce opacity-65 duration-75 rotate-5 absolute right-50 bottom-0"
+        />
+        <img
+          src="/ideas.png"
+          className="h-20 animate-bounce opacity-55 duration-75 rotate-5 absolute right-20 bottom-60"
+        />
+        <img
+          src="/brainstorm.png"
+          className="h-25 animate-bounce opacity-65 duration-75 rotate-350 absolute bottom-60 left-20"
+        />
         <img src={HomeImg} className="block mx-auto my-20 w-200" />
       </div>
-
+      <hr />
+      <hr />
       <div>
-        <p className="italic text-2xl font-bold text-center font-serif py-3 px-4 bg-amber-600 mb-15">
+        <p className="italic text-2xl font-bold text-center font-serif py-3 px-4 mb-15 my-5">
           JOIN US
         </p>
         <div className="flex justify-center gap-15">
@@ -71,6 +91,14 @@ function Home() {
           </div>
         </div>
       </div>
+      <hr />
+      <hr />
+
+      <div>
+        <img src={Pic1} className="my-15 h-120 block mx-auto" />
+      </div>
+      <hr />
+      <hr />
     </div>
   );
 }
