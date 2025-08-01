3c3c9b9210ac295deebbe39cc1653e3e534d539d
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-08-01 17:56:42 +0530

    Added localStorage Set

--- src/components/HackathonForm.jsx
+++ src/components/HackathonForm.jsx
@@ -7,16 +7,97 @@ import Swal from "sweetalert2";
 import toast, { Toaster } from "react-hot-toast";
 
 function HackathonForm() {
+  const [hackathonData, setHackathonData] = useState({
+    name: "",
+    location: "",
+    city: "",
+    startDate: "",
+    endDate: "",
+    time: "",
+    organizer: "",
+    description: "",
+    prizes: "",
+    rules: "",
+  });
+
+  const [errorName, setErrorName] = useState();
+  const [errorDescription, setErrorDescription] = useState();
+  const [errorDate, setErrorDate] = useState();
+  const [errorTime, setErrorTime] = useState();
+  const [errorOrganizer, setErrorOrganizer] = useState();
+  const [errorRules, setErrorRules] = useState();
+
+  useEffect(() => {
+    if (hackathonData.name === "") {
+      setErrorName("");
+    } else if (hackathonData.name.length < 3) {
+      setErrorName("Name cannot have less than 3 characters");
+    } else if (hackathonData.name.length > 30) {
+      setErrorName("Name cannot have more than 30 characters");
+    } else {
+      setErrorName("");
+    }
+
+    if (hackathonData.description.length === 0) {
+      setErrorDescription("");
+    } else if (hackathonData.description.length < 50) {
+      setErrorDescription("Description cannot have less than 50 characters");
+    } else if (hackathonData.description.length > 1000) {
+      setErrorDescription("Description cannot more than 1000 characters");
+    } else {
+      setErrorDescription("");
+    }
+
+    if (hackathonData.startDate === "" || hackathonData.endDate === "") {
+      setErrorDate("");
+    } else if (hackathonData.startDate > hackathonData.endDate) {
+      setErrorDate("End date cannot be before start date");
+    } else {
+      setErrorDate("");
+    }
+
+    if (hackathonData.organizer === "") {
+      setErrorOrganizer("");
+    } else if (hackathonData.organizer.length < 3) {
+      setErrorOrganizer("Name cannot have less than 3 characters");
+    } else if (hackathonData.organizer.length > 30) {
+      setErrorOrganizer("Name cannot have more than 30 characters");
+    } else {
+      setErrorOrganizer("");
+    }
+
+    if (hackathonData.rules.length === 0) {
+      setErrorRules("");
+    } else if (hackathonData.rules.length < 150) {
+      setErrorRules("Ruels & Regulations cannot have less than 150 characters");
+    } else if (hackathonData.rules.length > 1000) {
+      setErrorRules("Ruels & Regulations cannot more than 1000 characters");
+    } else {
+      setErrorRules("");
+    }
+  }, [
+    hackathonData.name,
+    hackathonData.description,
+    hackathonData.startDate,
+    hackathonData.endDate,
+    hackathonData.time,
+    hackathonData.organizer,
+    hackathonData.rules,
+  ]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (
       errorName ||
       errorDescription ||
+      errorDate ||
       errorTime ||
       errorOrganizer ||
       errorRules
     ) {
-      toast.error("Fix the error first.");
+      toast.error(
+        "Some fields are invalid or missing. Please check and try again."
+      );
       return;
     }
 
@@ -32,83 +113,33 @@ function HackathonForm() {
       toast.error("Please fill in all the required fields.");
       return;
     }
+    let existingHackathons =
+      JSON.parse(localStorage.getItem("hackathonData")) || [];
+
+    existingHackathons.push(hackathonData);
+
+    localStorage.setItem("hackathonData", JSON.stringify(existingHackathons));
 
     toast.success("Your data has been saved.");
+    setHackathonData({
+      name: "",
+      location: "",
+      city: "",
+      startDate: "",
+      endDate: "",
+      time: "",
+      organizer: "",
+      description: "",
+      prizes: "",
+      rules: "",
+    });
+    setErrorName("");
+    setErrorDescription("");
+    setErrorDate("");
+    setErrorTime("");
+    setErrorOrganizer("");
+    setErrorRules("");
   };
-
-  const [hackathonData, setHackathonData] = useState({
-    name: "",
-    location: "",
-    city: "",
-    startDate: "",
-    endDate: "",
-    time: "",
-    organizer: "",
-    description: "",
-    rules: "",
-  });
-
-  const [errorName, setErrorName] = useState();
-  const [errorDescription, setErrorDescription] = useState();
-  const [errorTime, setErrorTime] = useState();
-  const [errorOrganizer, setErrorOrganizer] = useState();
-  const [errorRules, setErrorRules] = useState();
-
-  useEffect(() => {
-    if (hackathonData.name === "") {
-      setErrorName("");
-    } else if (hackathonData.name.length < 3) {
-      setErrorName("Name cannot have less than 3 characters");
-    } else if (hackathonData.name.length > 30) {
-      setErrorName("Name cannot have more than 30 charaacters");
-    } else {
-      setErrorName("");
-    }
-
-    if (hackathonData.description.length === 0) {
-      setErrorDescription("");
-    } else if (hackathonData.description.length < 50) {
-      setErrorDescription("Description cannot have less than 50 characters");
-    } else if (hackathonData.description.length > 1000) {
-      setErrorDescription("Description cannot more than 1000 characters");
-    } else {
-      setErrorDescription("");
-    }
-
-    if (!hackathonData.time) {
-      setErrorTime("Time is required");
-    } else {
-      setErrorTime("");
-    }
-
-    if (hackathonData.organizer === "") {
-      setErrorOrganizer("");
-    } else if (hackathonData.organizer.length < 3) {
-      setErrorOrganizer("Name cannot have less than 3 characters");
-    } else if (hackathonData.organizer.length > 30) {
-      setErrorOrganizer("Name cannot have more than 30 characters");
-    } else {
-      setErrorOrganizer("");
-    }
-
-    if (hackathonData.rules.length === 0) {
-      setErrorRules("");
-    } else if (hackathonData.rules.length < 50) {
-      setErrorRules("Ruels & Regulations cannot have less than 50 characters");
-    } else if (hackathonData.rules.length > 1000) {
-      setErrorRules("Ruels & Regulations cannot more than 1000 characters");
-    } else {
-      setErrorRules("");
-    }
-  }, [
-    hackathonData.name,
-    hackathonData.description,
-    hackathonData.startDate,
-    hackathonData.endDate,
-    hackathonData.time,
-    hackathonData.organizer,
-    hackathonData.rules,
-  ]);
 
   return (
     <form
@@ -153,49 +184,51 @@ function HackathonForm() {
         />
         <span className="text-red-400">{errorDescription}</span>
       </div>
+      <div>
+        <div className="flex flex-col md:flex-row gap-6">
+          <div className="flex flex-col flex-1">
+            <Label
+              htmlFor={"start-date"}
+              labelTitle={"Start Date"}
+              important={true}
+            />
+            <InputField
+              type="date"
+              id="start-date"
+              name="start-date"
+              min={new Date().toISOString().split("T")[0]}
+              value={hackathonData.startDate}
+              onchange={(e) =>
+                setHackathonData((prev) => ({
+                  ...prev,
+                  startDate: e.target.value,
+                }))
+              }
+            />
+          </div>
 
-      <div className="flex flex-col md:flex-row gap-6">
-        <div className="flex flex-col flex-1">
-          <Label
-            htmlFor={"start-date"}
-            labelTitle={"Start Date"}
-            important={true}
-          />
-          <InputField
-            type="date"
-            id="start-date"
-            name="start-date"
-            min={new Date().toISOString().split("T")[0]}
-            value={hackathonData.startDate}
-            onchange={(e) =>
-              setHackathonData((prev) => ({
-                ...prev,
-                startDate: e.target.value,
-              }))
-            }
-          />
+          <div className="flex flex-col flex-1">
+            <Label
+              htmlFor={"end-date"}
+              labelTitle={"End Date"}
+              important={true}
+            />
+            <InputField
+              type="date"
+              id="end-date"
+              name="end-date"
+              min={new Date().toISOString().split("T")[0]}
+              value={hackathonData.endDate}
+              onchange={(e) =>
+                setHackathonData((prev) => ({
+                  ...prev,
+                  endDate: e.target.value,
+                }))
+              }
+            />
+          </div>
         </div>
-
-        <div className="flex flex-col flex-1">
-          <Label
-            htmlFor={"end-date"}
-            labelTitle={"End Date"}
-            important={true}
-          />
-          <InputField
-            type="date"
-            id="end-date"
-            name="end-date"
-            min={new Date().toISOString().split("T")[0]}
-            value={hackathonData.endDate}
-            onchange={(e) =>
-              setHackathonData((prev) => ({
-                ...prev,
-                endDate: e.target.value,
-              }))
-            }
-          />
-        </div>
+        <span className="text-red-400">{errorDate}</span>
       </div>
       <div className="flex flex-col">
         <Label htmlFor={"time"} labelTitle={"Time"} important={true} />
@@ -261,6 +294,10 @@ function HackathonForm() {
           id="prizes"
           name="prizes"
           placeholder="Total Prize Amount"
+          value={hackathonData.prizes}
+          onchange={(e) =>
+            setHackathonData((prev) => ({ ...prev, prizes: e.target.value }))
+          }
         />
       </div>
       <div className="flex flex-col">
--- src/components/HackthonList.jsx
+++ src/components/HackthonList.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import viewHackathonImg from "./../assets/view-hackathon.png";
 
 function HackthonList() {
+  const hacksInDashboard = JSON.parse(localStorage.getItem("hacksInDashboard")) || [];
   return (
-    <div className="text-center text-xl font-medium text-gray-800 w-full max-w-4xl bg-white rounded-2xl shadow-lg p-6 sm:p-8">
-      <img src={viewHackathonImg} alt="Image" className="w-70 mx-auto" />
-      No hackathons available. Try adding one!
-    </div>
+    <>
+
+      <div className="text-center text-xl font-medium text-gray-800 w-full max-w-4xl bg-white rounded-2xl shadow-lg p-6 sm:p-8">
+        <img src={viewHackathonImg} alt="Image" className="w-70 mx-auto" />
+        No hackathons available. Try adding one!
+      </div>
+    </>
   );
 }
 
