3bd6d88ad23ec6616ca52ff4824f59fdb520eae1
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-08-02 00:59:00 +0530

    Data added into Aboutconfig.js and Aboutcard.jsx

--- src/components/cards/Aboutcard.jsx
+++ src/components/cards/Aboutcard.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-function Aboutcard() {
+function Aboutcard( individualMember) {
   return (
-    <div>
-      
+
+          <div
+            className='px-30 py-6 rounded-2xl flex flex-col items-center text-center'
+            key={individualMember.name} // Using noun as key,as each noun is diff.//
+          >
+            <img
+              src={individualMember.profilephoto}
+              className='size-44 mb-4' 
+              alt={`${individualMember.noun}'s profile`}
+            />
+            <p className='py-2 px-6 bg-amber-400 rounded-lg text-lg font-semibold mb-2'>{individualMember.noun}</p>
+            <p className='text-gray-700 text-sm'>{individualMember.role}</p>
+            <div className='flex mt-4 gap-4'> 
+
+              {individualMember.social.map((socialLink, socialIndex) => (
+                <a
+                  href={socialLink.url}
+                  target='_blank'
+                  key={socialLink.url}>
+                  <img
+                    src={socialLink.icon}
+                    className='size-8 bg-gray-200 p-1 rounded-xl transition-transform duration-200 hover:scale-120 hover:bg-gray-300'
+                    alt={`${socialLink.noun} icon`}
+                  />
+                </a>
+              ))}
+    </div>
     </div>
   )
 }
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
 import { MEMBERS } from '../configs/Aboutconfig';
+import Aboutcard from '../components/cards/Aboutcard'
 
 function About() {
   return (
@@ -67,37 +68,14 @@ function About() {
 
 {MEMBERS.map((individualMember) => {
 
-  const{noun,role, profilephoto,social}=individualMember;
-  return(
-          <div
-            className='px-30 py-6 rounded-2xl flex flex-col items-center text-center'
-            key={individualMember.name} // Using noun as key,as each noun is diff.//
-          >
-            <img
-              src={individualMember.profilephoto}
-              className='size-44 mb-4' 
-              alt={`${individualMember.noun}'s profile`}
-            />
-            <p className='py-2 px-6 bg-amber-400 rounded-lg text-lg font-semibold mb-2'>{individualMember.noun}</p>
-            <p className='text-gray-700 text-sm'>{individualMember.role}</p>
-            <div className='flex mt-4 gap-4'> 
-
-              {individualMember.social.map((socialLink, socialIndex) => (
-                <a
-                  href={socialLink.url}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  key={socialLink.url} 
-                  aria-label={`Visit ${individualMember.noun}'s ${socialLink.noun|| 'social'} profile`}
-                >
-                  <img
-                    src={socialLink.icon}
-                    className='size-8 bg-gray-200 p-1 rounded-xl transition-transform duration-200 hover:scale-120 hover:bg-gray-300'
-                    alt={`${socialLink.noun} icon`}
-                  />
-                </a>
-              ))}
-      </div>   
+  const{profilephoto,noun,role,social}=individualMember;
+  return(   
+    <div>
+      <Aboutcard
+      profilephoto={profilephoto}
+      noun={noun}
+      role={role}
+      social={social}/>
       </div>
   )
 })}
