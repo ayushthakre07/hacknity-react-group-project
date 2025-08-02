5b39779c940b2e670c819c50219d3232af64b90f
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-08-02 08:44:33 +0530

    Config file created for trending projects

---
+++ src/configs/TrendingProjectsConfig.js
@@ -0,0 +1,29 @@
+  const trendingProjects = [
+    {
+      id: 1,
+      title: "ChillQuest",
+      description: "Interactive chill game project with NFT rewards",
+      likes: 311,
+      tags: ["gaming", "web3", "unity"],
+      team: "Pixel Pioneers",
+    },
+    {
+      id: 2,
+      title: "Matrix",
+      description:
+        "Learn new programming languages through interactive challenges",
+      likes: 135,
+      tags: ["education", "web"],
+      team: "Code Alchemists",
+    },
+    {
+      id: 3,
+      title: "EcoTrack",
+      description: "Carbon footprint tracker with blockchain verification",
+      likes: 278,
+      tags: ["sustainability", "blockchain"],
+      team: "Green Hackers",
+    },
+  ];
+
+  export default trendingProjects;
