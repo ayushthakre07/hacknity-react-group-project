d409725d04173f397d7bdd9713edcaa9dc86ff1a
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-08-02 14:54:33 +0530

    Items aligned in the centre in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,15 @@ import Button from '../components/Button'
 function Footer() {
   return (
     <div>
+      <br/>
       <br />
-      <hr className='border-dotted text-lime-500' />
-      <div className='text-center text-[30px] md:text[30px] font-extrabold text-gray-800'>Join us in our <span className='text-lime-500'>adventures</span> </div>
+      <hr className='border-dotted text-lime-500' />  <br/>
+      <div className='text-center md:text-[30px] font-extrabold text-gray-800'>Join us in our <span className='text-lime-500'>adventures</span> </div>
       <p className='text-center mt-4  md:text-[20px] mx-14 text-gray-500'>We invite you to be a part of the HACKNITY journey. Whether you're launching your dream game, discovering new worlds,
         or simply connecting with fellow enthusiasts,
         you'll find a welcoming home here. Explore our features,
         get started with your first host, or reach out to our team - we're excited to have you!</p>
-      <div className='text-center mt-4'>
-        <Button BtnTitle={"Get in touch"} />
-      </div>
-      <br />
+        <br />
       <br />
       <div className=' md:text-3xl font-bold mx-5 px-4 '>
         <h1 className='text-center'>For us, it's all about <span className='text-lime-400'>
@@ -21,33 +19,28 @@ function Footer() {
         </h1>
        <div/>
        <br/>
-       <div className='flex'>
+       <div className='text-center'>
         <div className='mt-5 px-4'>
       <p className='text-[16px] font-medium text-gray-400'> Engaging & Community-Focused <br/><br/>Join our community!</p>
       <br/>
-             <p className='flex '>      
-          <a href='https://www.instagram.com/' target='_blank'><img src='https://cdn-icons-png.flaticon.com/128/1384/1384031.png' className='size-[30px]' /></a>
-          <a href='https://github.com/' target='_blank'><img src='https://cdn-icons-png.flaticon.com/128/25/25231.png' className='size-[30px] ml-10' /></a>
-         <a href='https://www.roadtocode.org/' target='_blank'> <img src='https://cdn-icons-png.flaticon.com/128/1927/1927656.png' className='size-[30px] ml-10' /></a>
-          <a href='https://x.com/' target='_blank'><img src='https://cdn-icons-png.flaticon.com/128/5969/5969020.png' className='size-[30px] ml-10' /></a>
-         <a href='https://www.youtube.com/@roadtocode4u' target='_blank'> <img src='https://cdn-icons-png.flaticon.com/128/1077/1077046.png' className='size-[30px] ml-10' /></a>
-          </p>      
+             <div className='flex justify-center items-center mx-5 '>      
+          <a href='https://www.instagram.com/' target='_blank'><img src='https://cdn-icons-png.flaticon.com/128/1384/1384031.png' className='size-[30px] ml-5' /></a>
+          <a href='https://github.com/' target='_blank'><img src='https://cdn-icons-png.flaticon.com/128/25/25231.png' className='size-[30px] ml-5' /></a>
+         <a href='https://www.roadtocode.org/' target='_blank'> <img src='https://cdn-icons-png.flaticon.com/128/1927/1927656.png' className='size-[30px] ml-5' /></a>
+          <a href='https://x.com/' target='_blank'><img src='https://cdn-icons-png.flaticon.com/128/5969/5969020.png' className='size-[30px] ml-5' /></a>
+         <a href='https://www.youtube.com/@roadtocode4u' target='_blank'> <img src='https://cdn-icons-png.flaticon.com/128/1077/1077046.png' className='size-[30px] ml-5' /></a>
+          </div>      
         </div> 
-        <p className='text-[16px] font-medium text-gray-400'> Engaging & Community-Focused <br/><br/>Join our community!</p>
-        <br/>
-          
-
-
         </div>
-        <br/>
       </div>
       <br/>
       <br/>
+      
           <hr className='border-dotted '/>
-          <br/>
+          <br/><br/>
           
            <div>
-             <h1 className='text-lime-400 text-2xl font-extrabold ml-6'>HACKNITY</h1>
+             <h1 className='text-lime-400 text-2xl font-extrabold text-center'>HACKNITY</h1>
         <br/><br/><br/><br/> </div>
     </div>
 
